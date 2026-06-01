6db19355413c6bace7c23c7270e45659aa757e07
Philipp <philipp.eckhard.staudinger@gmail.com>
2026-06-01 23:20:12 +0200

    Fix Docker runtime errors: add protobuf dep and missing __init__.py files

--- worker/main.py
+++ worker/main.py
@@ -27,6 +27,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "proto"))
 
 import taskgrid_pb2
 from worker.worker import Worker
